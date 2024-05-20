a8b7c82f5b361a3ef91b12d81abcae3ba6d2122a
Achiya Haviv <ahiya.haviv@bewith.io>
2024-05-20 06:38:23 +0300

    prettier and eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,32 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended'],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', '^import\\s.+\\sfrom\\s.+;$'],
   rules: {
+    'no-console': 1,
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    semi: [2, 'always'],
+    'eol-last': ['error', 'always'],
+    curly: 2,
+    'max-len': [
+      'error',
+      {
+        code: 160,
+        tabWidth: 4,
+        ignorePattern: '^import\\s.+\\sfrom\\s.+;$',
+      },
+    ],
   },
 };
