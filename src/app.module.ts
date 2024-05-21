83845c83fceb4df66cc6ead3d44521cceccf3317
Achiya Haviv <ahiya.haviv@bewith.io>
2024-05-21 06:06:17 +0300

    base users and trainers resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { appConfig } from './configs/app.config';
+import { TrainersModule } from './trainers/trainers.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			load: [appConfig],
 		}),
+		TrainersModule,
+		UsersModule,
 	],
 	controllers: [AppController],
 	providers: [AppService],
